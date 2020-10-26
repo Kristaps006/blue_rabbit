61e9d2f18ff1fc2c5c6edc1b3e82cce41fe483f0
Kristaps006 <67914215+Kristaps006@users.noreply.github.com>
2020-10-26 14:22:28 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+
+<html lang="da">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/main.css">
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5rL9EncVBEV5s3rmVvB6zTRmgo5bgNSw" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory');?>/includes/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory');?>/includes/jquery.bxslider.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/main.js"></script>
+
+    <title>BLUE RABBIT</title>
+
+</head>
+
+<body>
+
+<div class="main-wrap container-fluid">
+
+    <div class="top-bar row">
+
+        <div class="container">
+
+            <div class="row">
+
+                <form class="navbar-form navbar-right search-form">
+
+                    <div class="form-group search-input-container">
+
+                        <input type="text" class="form-control search-input" placeholder="Search products">
+
+                    </div>
+
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <header>
+
+        <div class="row">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-6">
+
+                        <div class="logo-wrap pull-left">
+                            <img src="<?php bloginfo('template_directory');?>/img/logo.png" alt="Logo">
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6 navigation-menu">
+
+                        <div class="collapse navbar-collapse">
+
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Products <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="/index.php/shop/">Men</a></li>
+                                        <li><a href="/index.php/shop/">Women</a></li>
+                                        <li><a href="/index.php/shop/">Children</a></li>
+                                        <li><a href="/index.php/shop/">Category4</a></li>
+                                        <li><a href="/index.php/shop/">Category5</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="/index.php/about/">About</a></li>
+                                <li><a href="/index.php/contact/">Contact</a></li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </header>
+
+    <section class="not-found-section">
+
+        <div class="container">
+
+            <div class="col-lg-12">
+
+                <h1>The content you're looking for does not exist.</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Immo alio genere; Conferam tecum, quam cuique verso rem subicias; Quid est, quod ab ea absolvi et perfici debeat? Restinguet citius, si ardentem acceperit. Quamquam id quidem licebit iis existimare, qui legerint. Ut alios omittam, hunc appello, quem ille unum secutus est. Et quidem Arcesilas tuus, etsi fuit in disserendo pertinacior, tamen noster fuit; Duo Reges: constructio interrete.</p>
+
+            </div>
+
+        </div>
+
+    </section>
+
+</div> <!--\\ End main-wrap //-->
+
+<footer>
+
+    <div class="container">
+
+        <div class="col-lg-4">
+
+            <ul>
+
+                <li>
+                    <a href="#">Men</a>
+                </li>
+
+                <li>
+                    <a href="#">Women</a>
+                </li>
+
+                <li>
+                    <a href="#">Children</a>
+                </li>
+
+                <li>
+                    <a href="#">Category4</a>
+                </li>
+
+                <li>
+                    <a href="#">Category5</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+        <div class="col-lg-4">
+
+            <ul>
+
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+
+                <li>
+                    <a href="#">About</a>
+                </li>
+
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+        <div class="col-lg-4">
+
+            <p>
+                <strong>BLUE RABBIT</strong>
+            </p>
+
+            <p>
+                Prags Boulevard 49E <br>
+
+                DK-2300 <br>
+
+                København S <br>
+
+                <a href="#">info@html24.dk</a>
+
+            </p>
+
+        </div>
+
+    </div>
+
+</footer>
+
+<div class="copyright">
+
+    <div class="container">
+
+        <div class="col-lg-12">
+
+            © 2016 HTML24, All rights reserved
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
